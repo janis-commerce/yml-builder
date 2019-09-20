4adeb75dddcfb16f5d0f22496574362244551987
Nataniel López <nataniel.lopez@fizzmod.com>
2019-09-20 11:33:05 -0300

    Inited with package-generator

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { YmlBuilder } = require('./lib');
+
+module.exports = YmlBuilder;
---
+++ tests/yml-builder-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const YmlBuilder = require('./../index');
+
+const YmlBuilderError = require('./../lib/yml-builder-error')
+
+describe('YmlBuilder', () => {
+
+	// your tests here...
+
+});
