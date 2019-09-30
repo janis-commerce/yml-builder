0a749676bd35571107ed65d7b4d7991b2c083e93
Nataniel López <nataniel.lopez@fizzmod.com>
2019-09-30 11:10:28 -0300

    Tests fixes

--- tests/log-test.js
+++ tests/log-test.js
@@ -3,56 +3,67 @@
 const sandbox = require('sinon').createSandbox();
 const log = require('./../lib/utils/log');
 
+const stubLog = () => sandbox.stub(console, 'log');
+
 describe('log', () => {
 
 	afterEach(() => {
 		sandbox.restore();
 	});
 
-	describe('message', () => {
+	describe('message()', () => {
 
 		it('should send a simple log message', () => {
 
-			const consoleMock = sandbox.mock(console)
-				.expects('log')
-				.returns();
+			const stub = stubLog();
+
+			stub.returns();
 
 			log.message('some message');
 
-			consoleMock.verify();
+			sandbox.assert.calledOnce(stub);
 		});
+	});
+
+	describe('error()', () => {
 
 		it('should send an error log message', () => {
 
-			const consoleMock = sandbox.mock(console)
-				.expects('log')
-				.returns();
+			const stub = stubLog();
+
+			stub.returns();
 
 			log.error('some error message');
 
-			consoleMock.verify();
+			sandbox.assert.calledOnce(stub);
 		});
+	});
+
+	describe('warn()', () => {
 
 		it('should send a warning log message', () => {
 
-			const consoleMock = sandbox.mock(console)
-				.expects('log')
-				.returns();
+			const stub = stubLog();
+
+			stub.returns();
 
 			log.warn('some warning message');
 
-			consoleMock.verify();
+			sandbox.assert.calledOnce(stub);
 		});
+	});
+
+	describe('confirm()', () => {
 
 		it('should send a confirmaton log message', () => {
 
-			const consoleMock = sandbox.mock(console)
-				.expects('log')
-				.returns();
+			const stub = stubLog();
+
+			stub.returns();
 
 			log.confirm('some confirmation message');
 
-			consoleMock.verify();
+			sandbox.assert.calledOnce(stub);
 		});
 	});
 });
